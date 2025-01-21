d38d2f3a6b7042c17304d8d8ab62f951236e58e8
Umesh Gondia <UGondia.gs@emaar.ae>
2025-01-21 23:49:08 +0400

    Episode1

---
+++ App.js
@@ -0,0 +1,25 @@
+/*
+<div id="parent">
+     <div id="child">
+                    <h1>I'm H1 Tag</h1>
+                    <h2>I'm H2 Tag</h2>
+    </div>
+</div>
+*/
+
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"I'm H1 Tag"),React.createElement("h2",{},"I'm H2 Tag")]//for sibling you can create array like this
+    ));
+const heading = React.createElement(
+    "h1", 
+    { id: "heading", xyz: "abc" }, 
+    "Hello World from React !"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
